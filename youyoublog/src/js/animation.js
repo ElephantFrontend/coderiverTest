737f23ac2b488dfe1d694c9e91242a4e2f83dd43
ElephantFrontend <32238103+ElephantFrontend@users.noreply.github.com>
2017-10-02 23:06:42 +0300

    Add youyoublog

--- youyoublog/src/js/animation.js
+++ youyoublog/src/js/animation.js
@@ -7,3 +7,7 @@ TweenMax.staggerFrom('.js-coup', 1.5, {rotation: 180}, 0.1);
 TweenMax.staggerFrom('.js-emergence', 1.5, {scale:0}, 0.1);
 
 TweenMax.staggerFrom('.js-emergence_coup', 1.5, {scale:-1,rotation: 180}, 0.1);
+
+TweenMax.staggerFrom('.js-animat-left', 2, {zoom: 0,rotation: -80}, 0.1);
+
+TweenMax.staggerFrom('.js-animat-right', 2, {zoom: 0,rotation: 80}, 0.1);
